3cdf60eefc6e9708bef2a1a926f3c177a47a27e7
Belal0066 <belalanas2003@gmail.com>
2024-12-18 23:35:24 +0200

    no response yet

--- dnsServer.py
+++ dnsServer.py
@@ -1,18 +1,24 @@
 #dnsServer.py
 import logfromat as prntlog
 import socket
-
+import time
 
 
 port = 53
 ip = '127.0.0.66'
+ascii_art = '''
+╔╦╗╦ ╦╔═╗  ╔═╗╔═╗╦═╗╦  ╦╔═╗╦═╗  ╦ ╦╔═╗╔═╗  ╔═╗╔╦╗╔═╗╦═╗╔╦╗╔═╗╔╦╗
+ ║ ╠═╣║╣   ╚═╗║╣ ╠╦╝╚╗╔╝║╣ ╠╦╝  ╠═╣╠═╣╚═╗  ╚═╗ ║ ╠═╣╠╦╝ ║ ║╣  ║║
+ ╩ ╩ ╩╚═╝  ╚═╝╚═╝╩╚═ ╚╝ ╚═╝╩╚═  ╩ ╩╩ ╩╚═╝  ╚═╝ ╩ ╩ ╩╩╚═ ╩ ╚═╝═╩╝
+'''
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 try:
     sock.bind((ip, port))
-    prntlog.success_message(f"\nTHE SERVER HAS STARTED!!!\nip: {ip}, port {port}\n")
+    prntlog.success_message(f"\n{ascii_art}ip: {ip}, port {port}\n")
 except Exception as e:
     prntlog.error_message(f"ERROR BINDING TO {port}: {e}\n")
+    time.sleep(0.00001)
     exit(1)
 
 while True:
@@ -20,7 +26,7 @@ while True:
 
     data, addr = sock.recvfrom(512)
     prntlog.info_message_timed(f" <-- Received data from {addr}")
-    
+    sock.sendto(b"ma3ak a el dns ya bash!", addr)    
 
 
 
